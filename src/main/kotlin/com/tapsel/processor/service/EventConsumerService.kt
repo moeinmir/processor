b9013405b70e09516a61e2f6ff631b9dbb3bee42
moein  mirani <89993171+moeinmir@users.noreply.github.com>
2026-06-23 13:56:41 +0330

    Refactor EventConsumerService for click event handling

--- src/main/kotlin/com/tapsel/processor/service/EventConsumerService.kt
+++ src/main/kotlin/com/tapsel/processor/service/EventConsumerService.kt
@@ -1,23 +1,27 @@
-//package com.tapsel.processor.service
-//
-//@Component
-//class EventConsumerService(
-//    private val redisClient: RedisClient,
-//    private val bucketHelper: TimeBucketHelper
-//) {
-//
-//    @KafkaListener(topics = ["impression-topic"])
-//    fun consume(event: EventDto) {
-//
-//        val bucket = bucketHelper.bucketFromEpochMillis(event.eventTime)
-//
-//        val key = "impression:$bucket:${event.impressionId}"
-//
-//        redisClient.setWithTtl(
-//            key,
-//            event.toCompactString(),
-//            15 * 60 // 15 min TTL
-//        )
-//    }
-//}
-//
+package com.tapsel.processor.service
+
+import com.tapsel.common.constant.EventType
+import com.tapsel.common.dto.EventDto
+import com.tapsel.processor.redis.RedisClient
+import org.springframework.stereotype.Component
+
+import com.tapsel.common.util.TimeBucketHelper.Companion.bucketFromEpochMillis
+import org.springframework.kafka.annotation.KafkaListener
+
+@Component
+class EventConsumerService(
+    private val redisClient: RedisClient,
+    private val eventRedisHandlerService: EventRedisHandlerService,
+    private val adCrudOperationService : AdCrudOperationService
+
+) {
+
+    @KafkaListener(topics = ["click-topic"])
+    fun consume(event: EventDto) {
+        val bucket = bucketFromEpochMillis(event.eventTime)
+        val pair = eventRedisHandlerService.read(bucket,event.impressionId!!)
+        val contract = adCrudOperationService.getContractsByEventDto(pair!!)?.payOrGetPayed(EventType.Click)
+        adCrudOperationService.saveContract(contract!!)
+    }
+}
+
